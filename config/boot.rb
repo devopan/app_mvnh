4958439db7c7237a74c4ac78c8b5fad23559927f
Panagiotis Malamas <dpmal1@live.com>
2022-04-12 14:19:48 +0100

    Change env to development

--- config/boot.rb
+++ config/boot.rb
@@ -1,5 +1,5 @@
 # Set up gems listed in the Gemfile.
 ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../../Gemfile', __FILE__)
-ENV['RAILS_ENV'] == 'production'
+ENV['RAILS_ENV'] == 'development'
 
 require 'bundler/setup' if File.exists?(ENV['BUNDLE_GEMFILE'])
