3a209b7dc9e6c5a5049fe406ca77a54cd4b2196c
Panagiotis Malamas <dpmal1@live.com>
2022-04-12 23:48:39 +0100

    Removed admin classes

--- app/models/admin_user.rb
+++ app/models/admin_user.rb
@@ -17,9 +17,9 @@
 #  updated_at             :datetime
 #
 
-class AdminUser < ActiveRecord::Base
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, 
-         :recoverable, :rememberable, :trackable, :validatable
-end
+#class AdminUser < ActiveRecord::Base
+#  # Include default devise modules. Others available are:
+#  # :confirmable, :lockable, :timeoutable and :omniauthable
+#  devise :database_authenticatable, 
+#         :recoverable, :rememberable, :trackable, :validatable
+#end
