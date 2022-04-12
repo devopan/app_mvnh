8b4613a30c8b865cf2e4f981cb5cf3239569cf69
Panagiotis Malamas <dpmal1@live.com>
2022-04-12 23:49:46 +0100

    env variable

--- config/boot.rb
+++ config/boot.rb
@@ -1,6 +1,6 @@
 # Set up gems listed in the Gemfile.
 ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../../Gemfile', __FILE__)
 ENV['RAILS_ENV'] == 'development'
-ENV['{PORT:-5000'] == '5000'
+ENV['{PORT:-5000}'] == '5000'
 
 require 'bundler/setup' if File.exists?(ENV['BUNDLE_GEMFILE'])
